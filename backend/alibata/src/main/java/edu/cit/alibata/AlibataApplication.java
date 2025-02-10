a4aff31208b09f6e580afe447caa641cf0ee663a
Abel-O2 <samuel.abrenica01@gmail.com>
2025-02-10 19:23:23 +0800

    Add files via upload

---
+++ backend/alibata/src/main/java/edu/cit/alibata/AlibataApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.alibata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlibataApplication.class, args);
+	}
+
+}

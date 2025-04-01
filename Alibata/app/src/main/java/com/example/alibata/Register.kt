e17eaceeb4843640f327ed1d0cf860e7955633f8
InFinacity <yevrahznah@yahoo.com>
2025-04-01 22:51:43 +0800

    Android Frontend partial

---
+++ Alibata/app/src/main/java/com/example/alibata/Register.kt
@@ -0,0 +1,29 @@
+package com.example.alibata
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnReturn = findViewById<ImageButton>(R.id.btnRegReturn)
+        btnReturn.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+}

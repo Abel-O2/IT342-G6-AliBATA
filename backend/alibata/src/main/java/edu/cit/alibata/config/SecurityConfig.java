cc2456d1ff5ef6b39d118764ece18aee0bc3021a
Abel-O2 <samuel.abrenica01@gmail.com>
2025-04-07 00:24:41 +0800

    Added Documentation

--- backend/alibata/src/main/java/edu/cit/alibata/config/SecurityConfig.java
+++ backend/alibata/src/main/java/edu/cit/alibata/config/SecurityConfig.java
@@ -42,7 +42,19 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/alibata/auth/**")
+                .requestMatchers(
+                    "/api/alibata/auth/**",
+                    "/v2/api-docs",
+                    "/v3/api-docs",
+                    "/v3/api-docs/**",
+                    "/swagger-resources",
+                    "/swagger-resources/**",
+                    "/configuration/ui",
+                    "/configuration/security",
+                    "/swagger-ui/**",
+                    "/webjars/**",
+                    "/swagger-ui.html"
+                )
                 .permitAll()
                 //activities
                 //.requestMatchers("/api/alibata/activities/**").hasAnyRole(ADMIN.name())

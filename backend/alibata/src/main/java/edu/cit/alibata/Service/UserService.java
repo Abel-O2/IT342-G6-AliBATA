8111627a22708728eb762ffbe013c635819f6771
Abel-O2 <samuel.abrenica01@gmail.com>
2025-04-26 21:40:33 +0800

    Fixed User Password Bug

--- backend/alibata/src/main/java/edu/cit/alibata/Service/UserService.java
+++ backend/alibata/src/main/java/edu/cit/alibata/Service/UserService.java
@@ -55,7 +55,7 @@ public class UserService {
             user.setMiddleName(newUser.getMiddleName());
             user.setLastName(newUser.getLastName());
             user.setEmail(newUser.getEmail());
-            if (!passwordEncoder.matches(newUser.getPassword(), user.getPassword())) {
+            if (!passwordEncoder.matches(newUser.getPassword(), user.getPassword()) && !newUser.getPassword().startsWith("$2a$")) {
                 user.setPassword(passwordEncoder.encode(newUser.getPassword()));
             }
             user.setSubscriptionStatus(newUser.getSubscriptionStatus());
